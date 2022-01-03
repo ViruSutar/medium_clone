09b0a5e1b28b75e43cac92752bd2e69b2daad6c1
ViruSutar <virajsutar2402@gmail.com>
2022-01-03 08:08:55 +0530

    conflits

--- database/migrations/030_articles.ts
+++ database/migrations/030_articles.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import BaseSchema from "@ioc:Adonis/Lucid/Schema";
-=======
 import Database from '@ioc:Adonis/Lucid/Database'
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
->>>>>>> 8ddb95420d03e677cbae2c2b67f4753ecbcfc579
 
 // TODO: limit the number of images in arrray
 export default class Articles extends BaseSchema {
@@ -11,24 +7,6 @@ export default class Articles extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-<<<<<<< HEAD
-      table.increments("id").notNullable();
-      table.string("title", 100).notNullable();
-      table.integer("user_id").unsigned().notNullable();
-      table.text("content", "longtext").notNullable();
-      table.bigInteger("likes_count").defaultTo(0);
-      table.integer("reading_time").notNullable();
-      table.boolean("is_active").defaultTo(true);
-
-      table.index(["user_id"], "fk_article_users_idx");
-
-      table
-        .foreign("user_id", "fk_articles_users_idx")
-        .references("id")
-        .inTable("users")
-        .onDelete("no action")
-        .onUpdate("no action");
-=======
       table.uuid('id').notNullable().unique().defaultTo(Database.knexRawQuery("(UUID())"))
       table.string('title',100).notNullable()
       table.string('author_id').notNullable()
@@ -47,7 +25,6 @@ export default class Articles extends BaseSchema {
       .inTable("users")
       .onDelete('no action')
       .onUpdate('no action')
->>>>>>> 8ddb95420d03e677cbae2c2b67f4753ecbcfc579
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
