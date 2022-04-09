5e2401280512e51484699b86b28b9f394d03a3b3
ViruSutar <virajsutar2402@gmail.com>
2022-04-09 23:53:47 +0530

    alot changes done

--- app/Controllers/Http/TagsController.ts
+++ app/Controllers/Http/TagsController.ts
@@ -4,20 +4,7 @@ import TagService from "App/Services/TagService";
 import TagValidator from "App/Validators/TagValidator";
 
 export default class TagsController {
-  public async createTag({ request, response }) {
-    let { tag_name, image_link, description } = request.all();
-    let user_uuid = request.user.user_uuid;
-
-    await request.validate(TagValidator.createTag);
-    let tag = await TagService.createTag(
-      tag_name,
-      user_uuid,
-      image_link,
-      description
-    );
-
-    return response.status(200).send({ success: true, tagId: tag.tagId });
-  }
+  
 
   public async searchTag({ request, response }) {
     let { tag_name } = request.all();
@@ -62,55 +49,7 @@ export default class TagsController {
       return response.send({success:true,Data:tags.tags,count:tags.total})
   }
 
-  public async deleteTag({ request, response }) {
-    let { tag_id } = request.all();
-    let user_uuid = request.user.user_uuid;
+  
 
-    let tag = await TagService.deleteTag(tag_id, user_uuid);
-
-    if (tag.success === false) {
-      return response
-        .status(tag.status_code)
-        .send({ success: false, message: tag.message });
-    }
-
-    return response.send({ success: true, message: tag.message });
-  }
-
-  public async updateTag({request,response}){
-      let {tag_id, tag_name, status,image_link}=request.all()
-
-      await request.validate(TagValidator.updateTag)
-
-      let tag=await TagService.updateTag(tag_id, tag_name, status,image_link)
-
-      if(tag?.success === false){
-          return response.status(tag.status_code).send({success:false,message:tag.message})
-      }
-
-      return response.send({success:true,messsage:tag?.message})
-  }
-
-  public async listTagsAdmin({request,response}){
-    let {limit,offset,status,date}=request.all()
-    let user_uuid = request.user.user_uuid;
-   
-    let tags=await TagService.listTagsAdmin(user_uuid,status,limit,offset,date)
-
-    return response.send({success:true,tags:tags.tags})
-  }
-
-  public async rejectTag({request,response}){
-    let {tag_id,rejection_note}=request.all()
-    
-    await request.validate(TagValidator.rejectTag)
-
-    let tag=await TagService.rejectTag(tag_id,rejection_note)
-
-    if(tag?.success === false){
-      return response.status(tag.status_code).send({success:false,message:tag.message})
-    }
-
-    return response.send({success:true})
-  }
+  
 }
